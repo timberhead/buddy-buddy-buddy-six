30c1fcb6ee93b7febfa2ac63d6992cef497ac368
Al Wong <alwong.@Als-MacBook-Pro.local>
2022-08-18 17:39:20 -0600

    update script.js

--- Assets copy/js/script.js
+++ Assets copy/js/script.js
@@ -1,41 +1,60 @@
 
+var currentcity = document.querySelector("#current-city")
 
 
 
+//  https://api.openweathermap.org/geo/1.0/direct?q=Denver&limit=5&appid=dc33e42431b8cb4dcd79b6089776f818
 
 
+function fetchcoord() {
 
-var tableBody = document.getElementById('repo-table');
-var fetchButton = document.getElementById('fetch-button');
+    var api = "https://api.openweathermap.org/geo/1.0/direct?q=Denver&limit=5&appid=dc33e42431b8cb4dcd79b6089776f818"
+    fetch(api)
+        .then(function (res) {
 
-function getApi() {
-  
-  var requestUrl = 'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}';
+            return res.json()
 
-  fetch(requestUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data)
-      
-       for (var i = 0; i < data.length; i++) {
-         
-         var createTableRow = document.createElement('tr');
-         var tableData = document.createElement('td');
-         var link = document.createElement('a');
+        })
+        .then(function (data) {
+            console.log(data);
+            console.log(data[0].lat)
 
-         // Setting the text of link and the href of the link
-         link.textContent = data[i].html_url;
-         link.href = data[i].html_url;
+            var lat = data[0].lat
+            var lon = data[0].lon
 
-         // Appending the link to the tabledata and then appending the tabledata to the tablerow
-         // The tablerow then gets appended to the tablebody
-         tableData.appendChild(link);
-         createTableRow.appendChild(tableData);
-         tableBody.appendChild(createTableRow);
-      }
-    });
+            fetchweather(lat, lon)
+
+
+        })
+
+
 }
 
-fetchButton.addEventListener('click', getApi);
+function fetchweather(lat, lon) {
+
+    var api2 = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&cnt=5&appid=dc33e42431b8cb4dcd79b6089776f818`
+    fetch(api2)
+        .then(function (res) {
+
+            return res.json()
+
+        })
+        .then(function (data) {
+            console.log(data);
+
+            currentcity.textContent = data.name
+
+
+
+
+
+        })
+
+}
+
+function fetchuvi(){
+
+    // var api3 =
+}
+
+fetchcoord()
